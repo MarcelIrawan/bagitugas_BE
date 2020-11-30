3cfa3af63ad370dc1335998bfe0d76f2b3f91cea
Theofilus Marcel Irawan Sutomo <50488473+MarcelIrawan@users.noreply.github.com>
2020-11-30 11:51:38 +0700

    resolve deploy issue

--- deploy/update.sh
+++ deploy/update.sh
@@ -7,6 +7,6 @@ PROJECT_BASE_PATH='/usr/local/apps/bagi_tugas'
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
-supervisorctl restart bagiapi
+supervisorctl restart bagi_tugas
 
 echo "DONE! :)"
